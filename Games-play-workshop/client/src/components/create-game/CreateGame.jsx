6235db4d92d68adaa0eb44c8554d5bf6ae9df3b2
YordanDochev <yordan.dochev13@gmail.com>
2023-11-16 15:34:42 +0200

    Create game service

--- Games-play-workshop/client/src/components/create-game/CreateGame.jsx
+++ Games-play-workshop/client/src/components/create-game/CreateGame.jsx
@@ -1,4 +1,41 @@
+import { useState } from "react"
+
+const formInitialState = {
+    title: '',
+    category: '',
+    maxLevel: '',
+    imageUrl: '',
+    summary: '',
+
+}
 export default function CreateGame() {
+
+    const [formValues, setFormValues] = useState(formInitialState);
+
+    const changeHandler = (e) => {
+        let value = ''
+        switch (e.target.type) {
+            case 'number':
+                value = Number(e.target.value)
+                break;
+
+            default:
+                value = e.target.value
+
+        }
+
+        setFormValues(state=> ({
+            ...state,
+            [e.target.name]:value
+        }))
+    }
+
+    const onSubmitCreateGame = (e) => {
+        e.preventDefault();
+
+        console.log(formValues);
+    }
+
     return (
         <section id="create-page" className="auth">
             <form id="create">
@@ -6,20 +43,20 @@ export default function CreateGame() {
 
                     <h1>Create Game</h1>
                     <label htmlFor="leg-title">Legendary title:</label>
-                    <input type="text" id="title" name="title" placeholder="Enter game title..." />
+                    <input type="text" id="title" name="title" placeholder="Enter game title..." value={formValues.title} onChange={changeHandler}/>
 
                     <label htmlFor="category">Category:</label>
-                    <input type="text" id="category" name="category" placeholder="Enter game category..." />
+                    <input type="text" id="category" name="category" placeholder="Enter game category..." value={formValues.category} onChange={changeHandler}/>
 
                     <label htmlFor="levels">MaxLevel:</label>
-                    <input type="number" id="maxLevel" name="maxLevel" min="1" placeholder="1" />
+                    <input type="number" id="maxLevel" name="maxLevel" min="1" placeholder="1" value={formValues.maxLevel} onChange={changeHandler}/>
 
                     <label htmlFor="game-img">Image:</label>
-                    <input type="text" id="imageUrl" name="imageUrl" placeholder="Upload a photo..." />
+                    <input type="text" id="imageUrl" name="imageUrl" placeholder="Upload a photo..." value={formValues.imageUrl} onChange={changeHandler}/>
 
                     <label htmlFor="summary">Summary:</label>
-                    <textarea name="summary" id="summary"></textarea>
-                    <input className="btn submit" type="submit" value="Create Game" />
+                    <textarea name="summary" id="summary" value={formValues.summary} onChange={changeHandler} ></textarea>
+                    <input className="btn submit" type="submit" value="Create Game" onClick={onSubmitCreateGame}/>
                 </div>
             </form>
         </section>
