5bc3595e61667ce97f8ea186a54ef9666c51faa3
YordanDochev <yordan.dochev13@gmail.com>
2023-11-16 15:07:40 +0200

    Add register page

--- Games-play-workshop/client/src/App.jsx
+++ Games-play-workshop/client/src/App.jsx
@@ -3,6 +3,7 @@ import Home from "./components/home/Home"
 import Catalog from "./components/catalog/Catalog"
 import CreateGame from "./components/create-game/CreateGame"
 import Login from "./components/login/Login"
+import Register from "./components/register/Register"
 
 import {Routes,Route} from "react-router-dom"
 
@@ -17,6 +18,7 @@ function App() {
                 <Route path ="/catalog" element={<Catalog/>}/>
                 <Route path ="/games/create" element={<CreateGame/>}/>
                 <Route path ="/login" element={<Login/>}/>
+                <Route path ="/register" element={<Register/>}/>
             </Routes>
             
         </div>
