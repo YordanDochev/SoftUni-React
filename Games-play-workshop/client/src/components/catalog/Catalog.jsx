c86c13038eb9ffe0f0a9e40893d542de0ff36be4
YordanDochev <yordan.dochev13@gmail.com>
2023-11-16 16:07:57 +0200

    Show dynamic game list

--- Games-play-workshop/client/src/components/catalog/Catalog.jsx
+++ Games-play-workshop/client/src/components/catalog/Catalog.jsx
@@ -1,45 +1,24 @@
 import { useEffect, useState } from "react"
 
 import * as gameService from "../../services/gameService"
+import CatalogGameItem from "./catalog-game-item/CatalogGameItem"
 
 export default function Catalog() {
-    const [games,setGames] = useState([])
+    const [games, setGames] = useState([])
     useEffect(() => {
         gameService.getAll()
             .then(setGames)
-    },[])
-    console.log(games);
+    }, [])
+    
     return (
         <section id="catalog-page">
             <h1>All Games</h1>
-            <div className="allGames">
-                <div className="allGames-info">
-                    <img src="./images/avatar-1.jpg" />
-                    <h6>Action</h6>
-                    <h2>Cover Fire</h2>
-                    <a href="#" className="details-button">Details</a>
-                </div>
 
-            </div>
-            <div class="allGames">
-                <div class="allGames-info">
-                    <img src="./images/avatar-1.jpg" />
-                    <h6>Action</h6>
-                    <h2>Zombie lang</h2>
-                    <a href="#" class="details-button">Details</a>
-                </div>
-
-            </div>
-            <div class="allGames">
-                <div class="allGames-info">
-                    <img src="./images/avatar-1.jpg" />
-                    <h6>Action</h6>
-                    <h2>MineCraft</h2>
-                    <a href="#" class="details-button">Details</a>
-                </div>
-            </div>
-
-            <h3 class="no-articles">No articles yet</h3>
+            {games.map(game => (
+                <CatalogGameItem key={game._id} {...game} />
+            ))}
+            {games.length === 0 && <h3 class="no-articles">No articles yet</h3>}
+            
         </section>
     )
 }
