1f5782063de07c6d0abe83d61930b293fd231ea7
YordanDochev <yordan.dochev13@gmail.com>
2023-11-16 15:05:19 +0200

    Add create game page

--- Games-play-workshop/client/src/App.jsx
+++ Games-play-workshop/client/src/App.jsx
@@ -1,6 +1,7 @@
 import Header from "./components/header/Header"
 import Home from "./components/home/Home"
 import Catalog from "./components/catalog/Catalog"
+import CreateGame from "./components/create-game/CreateGame"
 
 import {Routes,Route} from "react-router-dom"
 
@@ -13,6 +14,7 @@ function App() {
             <Routes>
                 <Route path ="/" element={<Home/>}/>
                 <Route path ="/catalog" element={<Catalog/>}/>
+                <Route path ="/games/create" element={<CreateGame/>}/>
             </Routes>
             
         </div>
